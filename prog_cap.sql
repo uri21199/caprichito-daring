e13a141739a5e1751ac7f6db065580c6f81fc7f4
Lautaro Bustos <uri21199@gmail.com>
2025-02-18 20:36:22 -0300

    test

--- prog_cap.sql
+++ prog_cap.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 04-01-2025 a las 17:27:46
+-- Tiempo de generación: 18-02-2025 a las 23:42:49
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -20,6 +20,28 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `prog_cap`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `branches`
+--
+
+CREATE TABLE `branches` (
+  `branch_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  `branchname` varchar(100) NOT NULL,
+  `mail` varchar(200) DEFAULT NULL,
+  `cuit` varchar(20) DEFAULT NULL,
+  `store_name` varchar(255) DEFAULT NULL,
+  `province` varchar(200) DEFAULT NULL,
+  `city` varchar(100) DEFAULT NULL,
+  `address` varchar(200) DEFAULT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `website` varchar(200) DEFAULT NULL,
+  `status` varchar(50) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 -- --------------------------------------------------------
 
@@ -60,6 +82,14 @@ CREATE TABLE `cart` (
   `user_id` int(11) NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `cart`
+--
+
+INSERT INTO `cart` (`cart_id`, `user_id`, `created_at`) VALUES
+(1, 1, '2025-02-16 22:54:54'),
+(2, 3, '2025-02-17 22:23:19');
 
 -- --------------------------------------------------------
 
@@ -123,10 +153,23 @@ CREATE TABLE `logs` (
 CREATE TABLE `orders` (
   `order_id` bigint(20) UNSIGNED NOT NULL,
   `user_id` int(11) NOT NULL,
+  `cart_id` int(11) NOT NULL,
   `total_price` decimal(10,2) NOT NULL,
-  `status` varchar(50) DEFAULT 'pending',
+  `status` enum('Pendiente','Confirmada','Cancelada','') DEFAULT 'Pendiente',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `orders`
+--
+
+INSERT INTO `orders` (`order_id`, `user_id`, `cart_id`, `total_price`, `status`, `created_at`) VALUES
+(1, 1, 1, 199.31, 'Cancelada', '2025-02-16 22:56:16'),
+(2, 1, 1, 95.87, 'Pendiente', '2025-02-17 11:42:20'),
+(3, 3, 2, 18.94, 'Pendiente', '2025-02-17 22:28:43'),
+(4, 3, 2, 5.98, 'Pendiente', '2025-02-17 22:37:45'),
+(5, 3, 2, 247.30, 'Pendiente', '2025-02-17 22:38:26'),
+(6, 3, 2, 8.99, 'Pendiente', '2025-02-17 22:41:12');
 
 -- --------------------------------------------------------
 
@@ -141,6 +184,30 @@ CREATE TABLE `order_item` (
   `quantity` int(11) NOT NULL DEFAULT 1,
   `price` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `order_item`
+--
+
+INSERT INTO `order_item` (`orderItem_id`, `order_id`, `product_id`, `quantity`, `price`) VALUES
+(1, 1, 3, 5, 104.95),
+(2, 1, 2, 9, 49.41),
+(3, 1, 4, 5, 44.95),
+(4, 2, 2, 1, 5.49),
+(5, 2, 1, 1, 10.99),
+(6, 2, 8, 3, 5.97),
+(7, 2, 4, 5, 44.95),
+(8, 2, 7, 1, 2.49),
+(9, 2, 10, 2, 25.98),
+(10, 3, 4, 1, 8.99),
+(11, 3, 8, 5, 9.95),
+(12, 4, 8, 1, 1.99),
+(13, 4, 6, 1, 3.99),
+(14, 5, 10, 5, 64.95),
+(15, 5, 5, 5, 127.45),
+(16, 5, 8, 5, 9.95),
+(17, 5, 4, 5, 44.95),
+(18, 6, 4, 1, 8.99);
 
 -- --------------------------------------------------------
 
@@ -222,11 +289,11 @@ CREATE TABLE `site_info` (
 
 INSERT INTO `site_info` (`id`, `key_name`, `value`) VALUES
 (1, 'facebook', 'https://www.facebook.com/caprichito'),
-(2, 'instagram', 'https://www.instagram.com/caprichito'),
+(2, 'instagram', 'https://www.instagram.com/pelotascaprichito/'),
 (3, 'whatsapp', 'https://wa.me/123458888'),
-(4, 'address', 'Av. Siempre Viva 742, Buenos Aires, Argentina'),
-(5, 'phone', '+54 11 2222 3333'),
-(6, 'email', 'contacto@caprichito.com');
+(4, 'address', 'Necochea 2332 - Ramos Mejía, Buenos Aires, Argentina'),
+(5, 'phone', '+54 11 4651-5279'),
+(6, 'email', 'ventas@didifas.com.ar');
 
 -- --------------------------------------------------------
 
@@ -247,6 +314,7 @@ CREATE TABLE `users` (
   `address` text DEFAULT NULL,
   `website` text DEFAULT NULL,
   `role` varchar(50) DEFAULT 'user',
+  `comprador` varchar(100) DEFAULT NULL,
   `status` varchar(50) DEFAULT 'active',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -255,34 +323,19 @@ CREATE TABLE `users` (
 -- Volcado de datos para la tabla `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `mail`, `password`, `cuit`, `store_name`, `province`, `city`, `phone`, `address`, `website`, `role`, `status`, `created_at`) VALUES
-(1, 'admin', 'admin@caprichito.com', '123456', '20-12345678-9', NULL, 'Buenos Aires', 'CABA', '+54 11 1234-5678', 'Av. Siempre Viva 742', 'https://www.caprichito.com', 'admin', 'active', '2025-01-02 00:32:03'),
-(2, 'tienda1', 'tienda1@mail.com', 'password1', '30-87654321-0', NULL, 'Córdoba', 'Córdoba Capital', '+54 351 555-1234', 'Calle Falsa 123', NULL, 'user', 'active', '2025-01-02 00:32:03'),
-(3, 'tienda2', 'tienda2@mail.com', 'password2', '27-11223344-5', NULL, 'Santa Fe', 'Rosario', '+54 341 678-5678', 'Bv. Oroño 456', 'https://www.tienda2.com', 'user', 'active', '2025-01-02 00:32:03'),
-(4, 'tienda3', 'tienda3@mail.com', 'password3', '23-44556677-1', NULL, 'Mendoza', 'Mendoza Capital', '+54 261 123-9876', 'Av. San Martín', NULL, 'user', 'active', '2025-01-02 00:32:03'),
-(5, 'tienda4', 'tienda4@mail.com', 'password4', '26-99887766-4', NULL, 'Buenos Aires', 'Mar del Plata', '+54 223 987-4567', 'Calle 100 N° 200', 'https://www.tienda4.com', 'user', 'active', '2025-01-02 00:32:03'),
-(6, 'tienda5', 'tienda5@gmail.com', 'pbkdf2:sha256:1000000$Grwp0ZDHMtamaqWV$e2c5ed644fc37b8237544c3cfc10d8adb85e57bd96e86e9a794e92ed2ce86036', '12-12345678-1', NULL, NULL, NULL, NULL, NULL, NULL, 'user', 'active', '2025-01-02 00:32:03'),
-(7, 'tienda6', 'tienda6@gmail.com', 'pbkdf2:sha256:1000000$WaFtWfrto8v9cjzT$69e1afe374b845e2072d3256b4720c26ab86ca20b0d3f3cfce79b495fc44db96', '99-12345678-9', NULL, NULL, NULL, NULL, NULL, NULL, 'user', 'active', '2025-01-02 00:32:03'),
-(8, 'tienda7', 'tienda7@gmail.com', 'pbkdf2:sha256:1000000$YPCVsmZoSzWy1z7X$468935e3d391655024e7adf0d6b5997fbde54c548f8239f769556d6ed29b24db', '55-55555555-5', NULL, NULL, NULL, NULL, NULL, NULL, 'user', 'active', '2025-01-02 00:32:03'),
-(9, 'tienda8', 'tienda8@gmail.com', 'pbkdf2:sha256:1000000$fOddwvmkDlKUzrbn$6e71013e5d266eb86b08132b2e943d8d243d3db9c77add0bc9459cd5ea9c3cbb', '22-33333333-4', NULL, NULL, NULL, NULL, NULL, NULL, 'user', 'active', '2025-01-02 00:32:03'),
-(10, 'tienda9', 'tienda9@gmail.com', 'pbkdf2:sha256:1000000$atlcQX7UmEjSvDw6$ad67fdad722a12d0258c6d8c0d31f899806d3c22892f533f1e719a7bfdc0fbb0', '44-77777777-8', NULL, NULL, NULL, NULL, NULL, NULL, 'user', 'active', '2025-01-02 00:32:03'),
-(11, 'tienda10', 'tienda10@gmail.com', 'pbkdf2:sha256:1000000$rP03OXZ3voqwxiiA$8ddc0f55bd49fd1cb8eaa88b7a4bd3869512ff8b560f4394bf5d039f706bc158', '20-42280441-3', NULL, 'Buenos Aires', 'Ezeiza', NULL, NULL, NULL, 'user', 'active', '2025-01-02 00:32:03'),
-(12, 'tienda11', 'tienda11@gmail.com', 'pbkdf2:sha256:1000000$pwLT7knQXFYDktfk$222fd805d375525dff888a25a814c813ec7248dccea2550ea2f878838f4108d9', '55-55555555-6', NULL, NULL, NULL, NULL, NULL, NULL, 'user', 'active', '2025-01-02 00:32:03'),
-(13, 'tienda12', 'tienda12@gmail.com', 'pbkdf2:sha256:1000000$dBiQKZIsWEITWmPp$15fa4dcf97d0b7a4980e05513efcb6b9de08a3f537dcfcf235eee97807ebf11b', '98-76543219-9', NULL, NULL, NULL, NULL, NULL, NULL, 'user', 'active', '2025-01-02 00:32:03'),
-(14, 'tienda13', 'tienda13@gmail.com', 'pbkdf2:sha256:1000000$ig09pfO3gV9wLVMH$d3c2044dbcb66a995efb915c5a9471c530ecf3f2fe9197e85f9e12688129df09', '66-66666666-6', NULL, NULL, NULL, NULL, NULL, NULL, 'user', 'active', '2025-01-02 00:32:03'),
-(15, 'tienda14', 'tienda14@gmail.com', 'pbkdf2:sha256:1000000$Nn4FT8eYfk7EwnyU$698469fb3043b3441ba8b8d647f08285d27380978498491dae70e6b1987b0e46', '20-42280441-2', NULL, NULL, NULL, NULL, NULL, NULL, 'user', 'active', '2025-01-02 00:32:03'),
-(16, '', 'tienda15@gmail.com', 'pbkdf2:sha256:1000000$sg44MFSrdGP9LET1$4a2d86346210cd9c10131c45b1e89c56d5dc72176b5a8aef2aacf33c639c61fa', '88-88888888-7', 'Tienda 15', '', '', NULL, NULL, NULL, 'user', 'active', '2025-01-02 23:46:26'),
-(17, '', 'tienda16@gmail.com', 'pbkdf2:sha256:1000000$hFuBfbURCn6KXQVj$6e40a98330bd46468b541d4dbfd921c745e890b8573c33ace0a3d480b67e5ac0', '15-25814725-9', 'Tienda 16', 'Buenos Aires', 'CABA', NULL, NULL, NULL, 'user', 'inactive', '2025-01-02 23:57:52'),
-(18, '', 'tienda17@gmail.com', 'pbkdf2:sha256:1000000$GhOVCL9dPsYHJDPh$5e2dbc45624a2de75bec46c4aefef5e244f44f755179683387a72b9d7b3e9cf0', '15-25814725-3', 'Tienda 17', '', '', NULL, NULL, NULL, 'user', 'active', '2025-01-03 00:41:09'),
-(19, '', 'tienda18@gmail.com', 'pbkdf2:sha256:1000000$bGF0fkT5AQh8ytaB$db78138e4b181f02ce2675c4d2db22121ece3c6290293724fb56e9a3f636f0cb', '98-12345678-2', 'Tienda 18', '', '', '116513205', NULL, NULL, 'user', 'active', '2025-01-03 00:49:28'),
-(20, '', 'tienda19@gmail.com', 'pbkdf2:sha256:1000000$eXqomzs73t9CLTxP$fd5cae7ba48b95890425f7d4fea02ddd1f8dc676401cd71355da94942cc79336', '98-12345678-0', 'Tienda 19', '', '', NULL, NULL, NULL, 'user', 'inactive', '2025-01-03 00:50:19'),
-(21, '', 'tienda20@gmail.com', 'pbkdf2:sha256:1000000$fTxmPuN31Nmb591k$b277bd9bb29bbf30a17e527d45e2a33d663a40eedddb0576deaf0379d0b65904', '98-12345678-7', 'Tienda 20', '', '', NULL, NULL, NULL, 'user', 'inactive', '2025-01-03 00:57:23'),
-(22, '', 'tienda21@gmail.com', 'pbkdf2:sha256:1000000$ynlL0PZEA06jmJ6q$4dd14dfd24da675113fc1643a4e95fd52b515a8c0915dda5a29e00bc6c52f61d', '98-12345678-6', 'Tienda 21', 'La Pampa', 'Santa Rosa', NULL, NULL, NULL, 'user', 'inactive', '2025-01-03 01:11:42'),
-(23, '', 'caprichito@gmail.com', 'pbkdf2:sha256:1000000$jPqyoe9E7YnFx6aa$44227b28ef35840c86c0e63072a2a96c54492dab0a4229b66a006432ec6b9468', '42-280441-3', 'Caprichito', 'Buenos Aires', 'CABA', '', '', '', 'admin', 'active', '2025-01-03 01:50:35');
+INSERT INTO `users` (`id`, `username`, `mail`, `password`, `cuit`, `store_name`, `province`, `city`, `phone`, `address`, `website`, `role`, `comprador`, `status`, `created_at`) VALUES
+(1, '', 'caprichito@gmail.com', 'pbkdf2:sha256:1000000$F1WStcUUvHb42DVU$244457cae96c8444392275ca5a7236cb3590320e8fa415a96719ceff43604221', '20422804413', 'Caprichito', 'CABA', 'CABA', NULL, NULL, NULL, 'admin', NULL, 'active', '2025-02-16 22:47:50'),
+(3, '', 'tienda1@gmail.com', 'pbkdf2:sha256:1000000$NFjI4Hy5Jv3fvoq3$97f4d70540e303d745cc6ae9a751bc1bacc2fe3301057360dd2627082f59cdf4', '22-33333333-4', 'Tienda 1', 'Buenos Aires', 'Buenos Aires', NULL, NULL, NULL, 'user', NULL, 'active', '2025-02-17 21:35:53');
 
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `branches`
+--
+ALTER TABLE `branches`
+  ADD PRIMARY KEY (`branch_id`);
 
 --
 -- Indices de la tabla `carousel`
@@ -362,6 +415,12 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `branches`
+--
+ALTER TABLE `branches`
+  MODIFY `branch_id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT de la tabla `carousel`
 --
 ALTER TABLE `carousel`
@@ -371,13 +430,13 @@ ALTER TABLE `carousel`
 -- AUTO_INCREMENT de la tabla `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `cart_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT de la tabla `cart_item`
 --
 ALTER TABLE `cart_item`
-  MODIFY `cartItem_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cartItem_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
 
 --
 -- AUTO_INCREMENT de la tabla `contacts`
@@ -395,13 +454,13 @@ ALTER TABLE `logs`
 -- AUTO_INCREMENT de la tabla `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `order_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `order_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `order_item`
 --
 ALTER TABLE `order_item`
-  MODIFY `orderItem_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `orderItem_id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
 -- AUTO_INCREMENT de la tabla `products`
@@ -425,7 +484,7 @@ ALTER TABLE `site_info`
 -- AUTO_INCREMENT de la tabla `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
